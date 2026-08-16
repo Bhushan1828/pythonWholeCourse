e849b59468c55580588736743286ea2292a9bd69
Bhushan Sheshrao Ingle <inglebhushan098@gmail.com>
2026-08-16 08:41:43 +0530

    add String example

---
+++ 01_basics/String.py
@@ -0,0 +1,9 @@
+name = "Bhushan Ingle"
+print(name[1:9])
+slice_name = name[0:7]
+print(slice_name)
+num_list = "0123456789"
+print(num_list[3:])
+print(num_list[:7])
+print(num_list[0:7:3])
+print(name.upper());
