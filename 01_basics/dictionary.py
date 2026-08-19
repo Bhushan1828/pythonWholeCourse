db9857a34e6633cf019080b698bbda6c59c09ca0
Bhushan Sheshrao Ingle <inglebhushan098@gmail.com>
2026-08-19 10:41:01 +0530

    add dictionary example

---
+++ 01_basics/dictionary.py
@@ -0,0 +1,11 @@
+chai_types = {
+  "Adrak": "zeaty",
+  "chilly": "Hot",
+  "Green": "Mild",
+  "Lemon": "Good"
+}
+print(chai_types);
+print(chai_types.get("Green"))
+
+for chai in chai_types:
+  print(chai, chai_types)
